349e58acbdcdb34222080f4b83b7ca6213968c18
Oleksandr Zakharchuk <oleksandr.zakharchuk.dev@gmail.com>
2024-01-08 01:44:56 +0200

    Remove comments

--- main/views.py
+++ main/views.py
@@ -6,6 +6,4 @@ def index(request):
     news = Articles.objects.all()
     row_count = (len(news) // 2) + 1
     row_count_range = range(0, row_count)
-    #news_range = [2] * row_count_range
-    #news_range = range(1, len(news), 2)
     return render(request, 'main/index.html', {'news': news, 'row_count_range': row_count_range})
